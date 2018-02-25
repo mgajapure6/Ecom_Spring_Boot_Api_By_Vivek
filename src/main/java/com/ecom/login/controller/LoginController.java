73e05d271ce679784a17c14f345e977fed170373
vivek <vivek@vivek-PC>
2018-02-25 05:27:20 -0800

    new Method Added

--- src/main/java/com/ecom/login/controller/LoginController.java
+++ src/main/java/com/ecom/login/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.ecom.login.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import com.ecom.common.repository.UserRepository;
 public class LoginController {
    @Autowired
    UserRepository userRepository;
+   
    @RequestMapping(value="/login", method=RequestMethod.POST)	
    public CommonResponse getLogin(@RequestBody UserModel userModel) {
 	   String userName = userModel.getUserName();
@@ -24,6 +27,16 @@ public class LoginController {
 		   return new CommonResponse("Data Not Found", 404, null);
 	   }else {
 		   return new CommonResponse("Data Found", 200, userModel);
+	   }
+   }
+   
+   @RequestMapping(value="/findAllUser", method=RequestMethod.GET)	
+   public CommonResponse findAllUser() {
+	   List<UserModel> userList = (List<UserModel>) userRepository.findAll();
+	   if(userList.size()<=0) {
+		   return new CommonResponse("Data Not Found", 404, null);
+	   }else {
+		   return new CommonResponse("Data Found", 200, userList);
 	   }
    }
 }

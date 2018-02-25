436bfa07dbc7448e1a8b7929a14e4b9dfd7482ca
mgajapure6 <mgajapure6@gmail.com>
2018-02-26 00:58:26 +0530

    Signed-off-by: mgajapure6 <mgajapure6@gmail.com>

--- src/main/java/com/ecom/common/repository/UserRepository.java
+++ src/main/java/com/ecom/common/repository/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import com.ecom.common.model.UserModel;
 import java.lang.String;
-import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserModel, Integer> {
